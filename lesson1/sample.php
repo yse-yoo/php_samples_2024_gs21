86d261847e09d944b50d94a9aa6671ff55a8d5d7
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-17 09:47:19 +0900

    update

--- lesson1/sample.php
+++ lesson1/sample.php
@@ -14,27 +14,30 @@ $users = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
+    <title><?= $title; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1><?= $title; ?></h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($users as $user) : ?>
+    <div class="container">
+        <h1><?= $title; ?></h1>
+        <table class="table">
+            <thead>
                 <tr>
-                    <td><?= $user['id']; ?></td>
-                    <td><?= $user['name']; ?></td>
+                    <th>ID</th>
+                    <th>Name</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user) : ?>
+                    <tr>
+                        <td><?= $user['id']; ?></td>
+                        <td><?= $user['name']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
