b2be4f1df7388f4e035ca0f54ec9f5a7fdb683e4
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-17 13:33:29 +0900

    update

---
+++ lesson2/connect_test.php
@@ -0,0 +1,15 @@
+<?php 
+// DB Connect（データベース接続）
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>MySQL Test</h1>
+</body>
+</html>
