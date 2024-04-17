85c3e8e6e89e59000f7f76d0d9a82209856106ec
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-17 09:20:27 +0900

    update

--- lesson1/sample.php
+++ lesson1/sample.php
@@ -10,14 +10,31 @@ $users = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $title; ?></title>
 </head>
-<body>
-    <h2><?= $title ?></h2>    
 
-    <!-- Tableタグを使って $users の一覧を作ってください -->
+<body>
+    <h1><?= $title; ?></h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user) : ?>
+                <tr>
+                    <td><?= $user['id']; ?></td>
+                    <td><?= $user['name']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
+
 </html>
