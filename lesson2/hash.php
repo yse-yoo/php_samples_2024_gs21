c7a2d15a9f8d634da69626362a5f0add1fd08f1c
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-17 11:13:19 +0900

    update

---
+++ lesson2/hash.php
@@ -0,0 +1,43 @@
+<?php
+if (isset($_POST)) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hash_password = hash("sha256", $password);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email">
+        </div>
+
+        <div>
+            <label for="">Password</label>
+            <input type="password" name="password">
+        </div>
+        <div>
+            <button>Send</button>
+        </div>
+    </form>
+
+    <h2>Result</h2>
+    <h3>Email</h3>
+    <p><?= @$email ?></p>
+    <h3>Password</h3>
+    <p><?= @$password ?></p>
+    <h3>Hash Password</h3>
+    <p><?= @$hash_password ?></p>
+</body>
+
+</html>
